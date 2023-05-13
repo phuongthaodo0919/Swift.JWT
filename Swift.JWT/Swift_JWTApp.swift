12c890522fde085721d419e857483f71efef685f
Salm Do <thaodo.usa@gmail.com>
2023-05-13 16:38:35 -0500

    Init proj

---
+++ Swift.JWT/Swift_JWTApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_JWTApp.swift
+//  Swift.JWT
+//
+//  Created by Salmdo on 5/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_JWTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
